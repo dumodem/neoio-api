e2500c876b2c1b798e220b6261eb85b1c5281aca
Stella Du <dustellame@yahoo.com>
2022-08-10 16:03:06 +0800

    change headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://neoio-api.vercel.app" },
+        ],
+      },
+    ]
+  },
+}
